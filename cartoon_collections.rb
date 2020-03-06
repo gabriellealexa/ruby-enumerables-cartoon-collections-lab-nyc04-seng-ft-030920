9e549c5e841636448789f3e2beb3a3703b2b83e8
Gabrielle Noel <gabby@gabriellealexa.com>
2020-03-06 05:08:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,9 +23,9 @@ def summon_captain_planet(planeteer_calls)
    planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-summon_captain_planet(planeteer_calls)
 
-def long_planeteer_calls# code an argument here
+
+def long_planeteer_calls(planeteer_calls)
   # Your code here
 end
 
